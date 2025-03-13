b38c07564a79f32e2ff0007eac553735355bc5a1
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 15:21:25 -0700

    Create python-for-kids.py

---
+++ python-for-kids.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Python for Kids")
+st.write("Python for Kids is a website were you can learn Python and become a Python-Pro! From print() to zip(), we got everything to make you **really** good at Python.")
