253505098ab4b068d29fa826d05fc3564df51a80
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 15:37:44 -0700

    Update Comment.py

--- Comment.py
+++ Comment.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title("Commemnts")
 st.write("A comment can be written on any part of a python code, but don't put it in the middle of a piece of code. A comment starts with a # and looks like this:")
 st.code(body="# This is a comment", line_numbers=True)
+st.write("A comment does not interfere with any of the code. Check this out:")
+st.code(body="""
+x = 53
+#print("Hello")
+print(x)
+        """, line_numbers=True)
