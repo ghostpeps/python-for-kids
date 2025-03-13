7b35f139eee7db31200c97a911d6d30595086452
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 15:35:05 -0700

    Update python-for-kids.py

--- python-for-kids.py
+++ python-for-kids.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Python for Kids")
 link1 = "https://ghostpeps208.streamlit.app/Print"
 link2 = "https://ghostpeps208.streamlit.app/Zip"
-st.write("Python for Kids is a website were you can learn Python and become a Python-Pro! From [print()](%s)" % url1 + " to [zip()](%s)" % url2 + ", we got everything to make you *really* good at Python.")
+st.write("Python for Kids is a website were you can learn Python and become a Python-Pro! From [print()](%s)" % link1 + " to [zip()](%s)" % link2 + ", we got everything to make you *really* good at Python.")
