75b833b6ce9019667f9af44f6aa8d2ffce3f7854
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 15:56:22 -0700

    Update Comment.py

--- Comment.py
+++ Comment.py
@@ -3,12 +3,14 @@ import streamlit as st
 st.title("Commemnts")
 st.write("A comment can be written on any part of a python code, but don't put it in the middle of a piece of code. A comment starts with a # and looks like this:")
 st.code(body="# This is a comment", line_numbers=True)
+sr.code(body="", language=None)
 st.write("A comment does not interfere with any of the code. Check this out:")
 st.code(body="""
 x = 53
 #print("Hello")
 print(x)
         """, line_numbers=True)
+st.code(body="53", language=None)
 st.write("Comments can also be written on multiple lines using this: '''. Look at an example:")
 st.code(body="""
 '''
@@ -19,6 +21,13 @@ multiline
 comment.
 '''
         """, line_numbers=True)
+st.code(body="", language=None)
 st.write("Comments can also be written on the same line as a piece of code:")
-code = "print("There is a comment next to me!!!") # There is a print statement next to me!!!"
-st.code(body=code, line_numbers=True)
+st.code(body="""
+print("There is a comment next to me!!!") # There is a print statement next to me!!!
+        """, line_numbers=True)
+st.code(body="There is a comment next to me!!!", language=None)
+st.write("Please rate this site:")
+comment_feedback = st.feedback(options="stars")
+if comment_feedback is not None:
+        st.write("Thank you!")
