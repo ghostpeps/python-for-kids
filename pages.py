312afcb3dff7f1811c8d862dc83b00024bdbc648
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 20:12:55 -0700

    Update pages.py

--- pages.py
+++ pages.py
@@ -12,21 +12,23 @@ pages = {
         st.Page(page="Boolean.py", title="Booleans", icon=":material/check:"),
         st.Page(page="List.py", title="Lists", icon=":material/data_array:"),
         st.Page(page="Set.py", title="Sets", icon=":material/data_object:"),
-        st.Page(page="Tuple.py", title="Tuples"),
+        st.Page(page="Tuple.py", title="Tuples", icon=":material/workspaces:"),
         st.Page(page="Range.py", title="Ranges", icon=":material/exposure_plus_1:"),
         st.Page(page="Frozenset.py", title="Frozensets", icon=":material/ac_unit:"),
         st.Page(page="Dictionary.py", title="Dictionaries", icon=":material/dictionary:"),
-        st.Page(page="Byte.py", title="Bytes"),
-        st.Page(page="Bytearray.py", title="Bytearraies"),
+        st.Page(page="Byte.py", title="Bytes", icon=":material/brick:"),
+        st.Page(page="Bytearray.py", title="Bytearraies", icon=":material/grid_view:"),
         st.Page(page="Memoryview.py", title="Memoryviews", icon=":material/network_intelligence:"),
-        st.Page(page="Nonetype.py", title="NoneType (None)"),
+        st.Page(page="Nonetype.py", title="NoneType (None)", icon=":material/variables:"),
     ],
     "Comments": [
         st.Page(page="Comment.py", title="Comments", icon=":material/tag:"),
     ],
     "Print": [
         st.Page(page="Print.py", title="Print", icon=":material/print:"),
+    ],
+    "Variables": [
+        st.Page(page="Variable.py", title="Variables", icon=":material/close:")
     ]
-}
 pgs = st.navigation(pages)
 pgs.run()
