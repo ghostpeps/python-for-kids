f9ed03252a2446e8021049c9db1042d731348e37
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-13 19:36:41 -0700

    Update Print.py

--- Print.py
+++ Print.py
@@ -76,16 +76,16 @@ st.code(body="""
 Hi Same line!!!
   """, language=None)
 st.write("Backslashes are also helpful in Python:")
-body = """
-print(r"It\'s okay")
+body = r"""
+print("It\'s okay")
 print("This is a single backslash: \\")
 print("Hi\nNow I am on a new line")
-print(r"Look, \"Double Quotes\"")
-print(r"Hello\rHi")
+print("Look, \"Double Quotes\"")
+print("Hello\rHi")
 print("\tMake sure to indent")
 print("The following letter will dissapear: k\b")
-print(r"The following is an octal value: \110\145\154\154\157")
-print(r"The following is a hex value: \x48\x65\x6c\x6c\x6f")
+print("The following is an octal value: \110\145\154\154\157")
+print("The following is a hex value: \x48\x65\x6c\x6c\x6f")
   """
 st.code(body=body, line_numbers=True)
 st.write("Output")
