7ddf8ab0e4dacb4960c9f49077013f17c2e4620a
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-15 08:40:54 -0700

    Update Print.py

--- Print.py
+++ Print.py
@@ -85,33 +85,6 @@ st.write("Output")
 st.code(body="""
 Hi Same line!!!
   """, language=None)
-st.write("Backslashes are also helpful in Python:")
-body = r"""
-print("It\'s okay")
-print("This is a single backslash: \\")
-print("Hi\nNow I am on a new line")
-print("Look, \"Double Quotes\"")
-print("Hello\rHi")
-print("\tMake sure to indent")
-print("The following letter will dissapear: k\b")
-print("The following is an octal value: \110\145\154\154\157")
-print("The following is a hex value: \x48\x65\x6c\x6c\x6f")
-  """
-st.code(body=body, line_numbers=True)
-st.write("Output")
-st.code(body=r"""
-It's okay
-This is a single backslash: \
-Hi
-Now I am on a new line
-Look, "Double Quotes"
-Hello
-Hi
-	Make sure to indent
-The following letter will disappear: 
-The following is an octal value: Hello
-The following is a hex value: Hello
-  """, language=None)
 st.write("Please rate this site:")
 print_feedback = st.feedback(options="stars")
 total_stars = 0
