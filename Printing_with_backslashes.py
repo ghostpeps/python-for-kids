76054c58cd5c649b25a429fcfc086bf4f5c63563
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-03-15 20:51:22 -0700

    Update Printing_with_backslashes.py

--- Printing_with_backslashes.py
+++ Printing_with_backslashes.py
@@ -4,34 +4,6 @@ import math
 
 st.title("Printing with Backslashes")
 st.write(r"Printing with backslashes allows you to print things that you normally shouldn't. For example, \' prints as '. Look at an example:")
-'''
-body = r"""
-print("It\'s okay")
-print("This is a single backslash: \\")
-print("Hi\nNow I am on a new line")
-print("Look, \"Double Quotes\"")
-print("Hello\rHi")
-print("\tMake sure to indent")
-print("The following letter will dissapear: k\b")
-print("The following is an octal value: \110\145\154\154\157")
-print("The following is a hex value: \x48\x65\x6c\x6c\x6f")
-  """
-st.code(body=body, line_numbers=True)
-st.write("Output")
-st.code(body=r"""
-It's okay
-This is a single backslash: \
-Hi
-Now I am on a new line
-Look, "Double Quotes"
-Hello
-Hi
-	Make sure to indent
-The following letter will disappear: 
-The following is an octal value: Hello
-The following is a hex value: Hello
-  """, language=None)
-  '''
 st.code(body=r"It\'s okay", line_numbers=True)
 st.write("Output:")
 st.code(body="It\'s okay", language=None)
@@ -62,6 +34,92 @@ st.write("Output:")
 st.code(body=r"""
 Look, "Double Quotes"
   """, language=None)
+st.write(r"""\r gives a carriage return, similar to \n, a carriage return replaces the
+characters at the beginning of the current line and adds the remaining part
+to the end of the part after \r. Look at an example:""")
+st.code(body=r"""
+print("First part\rSecond")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body=r"""
+First part
+Second
+	""", language=None)
+st.write(r"""
+\t makes a tab or an indent wherever it is written:
+""")
+st.code(body=r"""
+print("\tMake sure to indent\tLook, another indent")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body=r"""
+	Make sure to indent	Look, another indent
+	""", language=None)
+st.write(r"""
+\b backspaces the previous character:
+	""")
+st.code(body=r"""
+print("Hello \bWorld!")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body="HelloWorld!", language=None)
+st.write(r"""
+\ooo where o represents numbers, \ooo is an octal value:
+	""")
+st.code(body=r"""
+print("The following is an octal value: \110\145\154\154\157")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body=r"""
+The following is an octal value: Hello
+	""", language=None)
+st.write(r"""
+\xhh where h represents numbers, \xhh is a hex value:
+	""")
+st.code(body=r"""
+print("The following is a hex value: \x48\x65\x6c\x6c\x6f")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body=r"""
+The following is a hex value: Hello
+	""", language=None)
+st.write(r"""
+\ucccc, where c represents a number or letter, \xcccc is a unicode:
+	""")
+st.code(body=r"""
+print("\u0041")
+print("\u03B1")
+print("\u2603")
+	""", line_numbers=True)
+st.write("Output:")
+st.code(body=r"""
+A
+α
+☃
+	""", language=None)
+st.write(r"""
+
+	""")
+st.code(body=r"""
+print("This is the first part.\fThis is the second part.")
+	""", line_numbers=True)
+st.write("Possible Output #1:")
+st.code(body=r"""
+This is the first part.
+This is the second part.
+	""", language=None)
+st.write("Possible Output #2:")
+st.code(body=r"""
+
+	""", language=None)
+st.write("Possible Output #3:")
+st.code(body=r"""
+This is the first part.This is the second part.
+	""", language=None)
+st.write("Possible Output #4:")
+st.code(body="""
+This is the first part.\u000cThis is the second part.
+	""", language=None)
 st.write("Please rate this site:")
 print_feedback = st.feedback(options="stars")
 total_stars = 0
